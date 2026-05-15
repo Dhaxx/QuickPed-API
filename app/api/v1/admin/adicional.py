da12b86da3e0e619c2be944cad0beff668406d2d
Kaio Pablo <69181318+Dhaxx@users.noreply.github.com>
2026-05-15 02:48:23 -0300

    Change permission checks from 'adicionais' to 'produtos'

--- app/api/v1/admin/adicional.py
+++ app/api/v1/admin/adicional.py
@@ -8,20 +8,20 @@ router = APIRouter()
 
 # Rotas dos Adicionais
 @router.post("/", response_model=AdicionalRead, status_code=status.HTTP_201_CREATED)
-def create_adicional(data: AdicionalCreate, session = Depends(get_session), estabelecimento_id: int = Depends(get_current_estabelecimento), _=Depends(require_permission("adicionais", "editar"))):
+def create_adicional(data: AdicionalCreate, session = Depends(get_session), estabelecimento_id: int = Depends(get_current_estabelecimento), _=Depends(require_permission("produtos", "editar"))):
     dados = data.model_dump()
     dados["estabelecimento_id"] = estabelecimento_id
     return adicional_service.create(session, dados)
 
 @router.get("/", response_model=list[AdicionalRead], status_code=status.HTTP_200_OK)
-def get_adicionais(session = Depends(get_session), estabelecimento_id: int = Depends(get_current_estabelecimento), grupo_id = int, _=Depends(require_permission("adicionais", "visualizar"))):
+def get_adicionais(session = Depends(get_session), estabelecimento_id: int = Depends(get_current_estabelecimento), grupo_id = int, _=Depends(require_permission("produtos", "visualizar"))):
     return adicional_service.get(session, estabelecimento_id, grupo_id)
 
 @router.put("/", response_model=AdicionalRead, status_code=status.HTTP_200_OK)
-def update_adicional(adicional_id: int, data: AdicionalUpdate, session = Depends(get_session), estabelecimento_id: int = Depends(get_current_estabelecimento), _=Depends(require_permission("adicionais", "editar"))):
+def update_adicional(adicional_id: int, data: AdicionalUpdate, session = Depends(get_session), estabelecimento_id: int = Depends(get_current_estabelecimento), _=Depends(require_permission("produtos", "editar"))):
     dados = data.model_dump(exclude_unset=True)
     return adicional_service.update(session, adicional_id, dados, estabelecimento_id)
 
 @router.delete("/", response_model=AdicionalRead, status_code=status.HTTP_200_OK)
-def delete_adicional(adicional_id: int, session = Depends(get_session), estabelecimento_id: int = Depends(get_current_estabelecimento), _=Depends(require_permission("adicionais", "editar"))):
+def delete_adicional(adicional_id: int, session = Depends(get_session), estabelecimento_id: int = Depends(get_current_estabelecimento), _=Depends(require_permission("produtos", "editar"))):
     return adicional_service.delete(session, adicional_id, estabelecimento_id)
